fb7321dbbf8c56606f4fd242f8d1b788a3222885
Bocilsetan <sean.filbert@binus.ac.id>
2023-11-29 07:26:09 +0700

    commit

--- secprogweb/index.php
+++ secprogweb/index.php
@@ -10,10 +10,10 @@
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
-     	<label>User Name</label>
+     	<label>Username</label>
      	<input type="text" name="uname" placeholder="User Name"><br>
 
-     	<label>User Name</label>
+     	<label>Username</label>
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
